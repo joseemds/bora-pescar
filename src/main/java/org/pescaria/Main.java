2602ee589d3915910c61bab928be7e8ce3144a8b
Jose Eduardo Monteiro <mds.josee@gmail.com>
2024-06-10 16:18:06 -0300

    ...

--- src/main/java/org/pescaria/Main.java
+++ src/main/java/org/pescaria/Main.java
@@ -1,10 +1,7 @@
 package org.pescaria;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-
-        }
-    }
+	public static void main(String[] args) {
+		System.out.println("salve");
+	}
 }
